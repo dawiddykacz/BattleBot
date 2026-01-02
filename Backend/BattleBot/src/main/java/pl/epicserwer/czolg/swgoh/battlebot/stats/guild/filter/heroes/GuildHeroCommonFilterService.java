48c33b11c1e75771ab4cc439f59a1a3c13e436a5
ddykacz <dawiddykaczd@wp.pl>
2026-01-02 12:25:41 +0100

    added logger to comlink requests

--- Backend/BattleBot/src/main/java/pl/epicserwer/czolg/swgoh/battlebot/stats/guild/filter/heroes/GuildHeroCommonFilterService.java
+++ Backend/BattleBot/src/main/java/pl/epicserwer/czolg/swgoh/battlebot/stats/guild/filter/heroes/GuildHeroCommonFilterService.java
@@ -5,6 +5,8 @@ import org.commons.AllyCode;
 import org.commons.Name;
 import org.commons.stats.guilds.GuildHeroes;
 import org.commons.stats.guilds.SortedHeroes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.epicserwer.czolg.swgoh.battlebot.stats.guild.GuildHeroesService;
 
 import java.util.List;
@@ -12,14 +14,21 @@ import java.util.List;
 class GuildHeroCommonFilterService implements GuildHeroFilterService{
     private final GuildHeroesService guildHeroesService;
     private final GuildHeroFilter guildHeroFilter;
+    private final Logger logger;
 
     public GuildHeroCommonFilterService(GuildHeroesService guildHeroesService, GuildHeroFilter guildHeroFilter) {
         this.guildHeroesService = guildHeroesService;
         this.guildHeroFilter = guildHeroFilter;
+
+        this.logger = LoggerFactory.getLogger(getClass());
     }
 
     public SortedHeroes getHeroesByRarity(@NonNull final AllyCode allyCode,@NonNull final List<Name> heroNames){
+        logger.info("Sorting heroes by Rarity from {}",allyCode);
         GuildHeroes guildHeroes = guildHeroesService.build(allyCode);
-        return guildHeroFilter.calculate(guildHeroes, heroNames);
+        logger.info("Sorting heroes by Rarity from {} - sorting",allyCode);
+        final SortedHeroes sortedHeroes = guildHeroFilter.calculate(guildHeroes, heroNames);
+        logger.info("Sorting heroes by Rarity from {} - completed",allyCode);
+        return sortedHeroes;
     }
 }
